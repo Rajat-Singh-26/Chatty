529993faa6c31022c7d9caa9350416dba6e6c6d5
unknown <rajatsinghsd@gmail.com>
2025-03-27 04:53:35 +0530

    update

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/chatty', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.log('Failed to connect to MongoDB:', err));
